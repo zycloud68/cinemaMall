5b4558e746267c94db2d90e05fb404eda494aebc
zycloud68 <yifan305>
2021-06-10 13:00:25 +0800

    影片模块中影片详情查询接口service和mapper编写

--- guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocActorTMapper.java
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocActorTMapper.java
@@ -1,7 +1,11 @@
 package com.stylefeng.guns.rest.common.persistence.dao;
 
+import com.stylefeng.guns.api.film.vo.ActorVO;
 import com.stylefeng.guns.rest.common.persistence.model.MoocActorT;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +17,5 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  */
 public interface MoocActorTMapper extends BaseMapper<MoocActorT> {
 
+    List<ActorVO> getActors(@Param("filmId") String filmId);
 }
--- guns-film/src/main/java/com/stylefeng/guns/rest/modular/film/service/DefaultFilmServiceImpl.java
+++ guns-film/src/main/java/com/stylefeng/guns/rest/modular/film/service/DefaultFilmServiceImpl.java
@@ -28,6 +28,10 @@ public class DefaultFilmServiceImpl implements FilmServiceApi {
     private MoocSourceDictTMapper moocSourceDictTMapper;
     @Autowired
     private MoocYearDictTMapper moocYearDictTMapper;
+    @Autowired
+    private MoocFilmInfoTMapper moocFilmInfoTMapper;
+    @Autowired
+    private MoocActorTMapper moocActorTMapper;
 
     /**
      * 获取首页信息
@@ -362,25 +366,57 @@ public class DefaultFilmServiceImpl implements FilmServiceApi {
     // 2. 根据影片id来获取电影信息的图片来源
     @Override
     public ImgVO getImgVo(String filmId) {
-        return null;
+        // 获取MoocFilmInfoT里面的数据
+        MoocFilmInfoT moocFilmInfoT = new MoocFilmInfoT();
+        // 根据filmId来获取信息
+        moocFilmInfoT.setFilmId(filmId);
+        // 调用持久层的信息
+        MoocFilmInfoT moocFilmInfoT1 = moocFilmInfoTMapper.selectOne(moocFilmInfoT);
+        String  filmStr = moocFilmInfoT1.getFilmImgs();
+        // 图片地址是按照五个逗号为分隔符连接的
+        String[] filmImgs= filmStr.split(",");
+        // 获取详细图片地址
+        ImgVO imgVO = new ImgVO();
+        imgVO.setMainImg(filmImgs[0]);
+        imgVO.setImg01(filmImgs[1]);
+        imgVO.setImg02(filmImgs[2]);
+        imgVO.setImg03(filmImgs[3]);
+        imgVO.setImg04(filmImgs[4]);
+        return imgVO;
     }
 
     // 3.1 根据影片id来获取电影导演信息
     @Override
     public ActorVO getDirectorVo(String filmId) {
-        return null;
+        MoocFilmInfoT moocFilmInfoT = new MoocFilmInfoT();
+        Integer directorId = moocFilmInfoT.getDirectorId();
+        // 调用持久层的信息
+        MoocActorT moocActorT = moocActorTMapper.selectById(directorId);
+        // 根据moocDirectInfo信息来查找director{imgAddress,directorName}
+        ActorVO actorVO = new ActorVO();
+        actorVO.setDirectorName(moocActorT.getActorName());
+        actorVO.setImgAddress(moocActorT.getActorImg());
+        return actorVO;
     }
 
     // 3.2 根据影片id来获取所有电影演员信息
     @Override
     public List<ActorVO> getActors(String filmId) {
-        return null;
+        List< ActorVO> actors = moocActorTMapper.getActors(filmId);
+        return actors;
     }
 
     // 4. 根据影片Id来获取电影详细描述信息
     @Override
     public FilmDescVO getFilmDescVo(String filmId) {
-        return null;
+        MoocFilmInfoT moocFilmInfoT = new MoocFilmInfoT();
+        moocFilmInfoT.setFilmId(filmId);
+        // 调用持久层信息
+        MoocFilmInfoT filmDescInfo = moocFilmInfoTMapper.selectOne(moocFilmInfoT);
+        FilmDescVO filmDescVO = new FilmDescVO();
+        filmDescVO.setBiography(filmDescInfo.getBiography());
+        filmDescVO.setFilmId(filmId);
+        return filmDescVO;
     }
 
 }
